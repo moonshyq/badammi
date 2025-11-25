967251ebd5a1c3ac2fde3acdb3a60e917d39cf2e
moonshyq <akasiyapika@gmail.com>
2025-11-25 18:23:15 +0400

    Add new element

--- BadammiAPI/obj/Debug/net9.0/BadammiAPI.AssemblyInfo.cs
+++ BadammiAPI/obj/Debug/net9.0/BadammiAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadammiAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9013aa7dbd497465b977dbfeb5114f3a3fd54d6c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87c4c4e8d1bb2d8785885a848d224c4190a8b73c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadammiAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadammiAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
